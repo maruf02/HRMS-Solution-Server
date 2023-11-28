8386e4cc787ffe8512ee9ea8e7c149834e1a31e7
maruf02 <rashiduzzaman99@gmail.com>
2023-11-28 23:47:13 +0600

    Note:  admin,HR,Employee  middleware get operation complete

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         const AllUsersCollection = client.db('HRMS').collection('UsersInfo');
+        const AllWorkSheetCollection = client.db('HRMS').collection('WorkSheet');
 
         // auth related api
         // Jwt for cookies
@@ -321,6 +322,200 @@ async function run() {
         })
 
         // All Users Api
+
+
+
+        // Employee worksheet/submitted task api
+
+
+        app.get('/worksheet', async (req, res) => {
+            const result = await AllWorkSheetCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+
+        // app.get('/worksheetemail/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     // console.log(role);
+        //     const query = { email: email }
+        //     const result = await AllWorkSheetCollection.find(query).toArray();
+        //     res.send(result);
+        // })
+        app.get('/worksheet/:email', async (req, res) => {
+            const email = req.params.email;
+            // console.log(role);
+            const query = { email: email }
+            const result = await AllWorkSheetCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+        app.get('/worksheet/:email/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await AllWorkSheetCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        // app.get('/worksheet/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     // console.log(id);
+        //     const query = { _id: new ObjectId(id) }
+        //     // console.log(query);
+        //     const result = await AllWorkSheetCollection.findOne(query);
+        //     // console.log(result);
+        //     res.send(result);
+        // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+        app.post('/worksheet', async (req, res) => {
+            const newWorkSheet = req.body;
+            console.log(newWorkSheet);
+            const result = await AllWorkSheetCollection.insertOne(newWorkSheet);
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Employee worksheet/submitted task api
+
+        // ekjon employee kotota project + koto hour kaj korce +totalmain salary+ total overtime salary+totalsalary, 
+
+        app.get('/employeeSummary/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const aggregateResult = await AllWorkSheetCollection.aggregate([
+                { $match: { email: email } },
+                {
+                    $group: {
+                        _id: '$email',
+                        totalWorks: { $sum: 1 },
+                        totalHours: { $sum: { $toInt: '$hours' } },
+                        totaloverHours: { $sum: { $toInt: '$overtime' } },
+                        totalUniqueDaysWorked: { $addToSet: { $dateFromString: { dateString: '$date' } } },
+                        totalMainSalary: { $sum: { $toDouble: '$mainSalary' } },
+                        totalOvertimeSalary: { $sum: { $toDouble: '$overtimeSalary' } },
+                        totalOvertimeSalary: { $sum: { $toDouble: '$overtimeSalary' } },
+
+
+                    }
+                },
+                {
+                    $addFields: {
+                        totalSalary: { $add: ['$totalMainSalary', '$totalOvertimeSalary'] }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        totalUniqueDaysWorked: { $size: '$totalUniqueDaysWorked' },
+                        _id: 1,
+                        totalWorks: 1,
+                        totalHours: 1,
+                        totaloverHours: 1,
+                        totalUniqueDaysWorked: { $size: '$totalUniqueDaysWorked' },
+                        totalMainSalary: 1,
+                        totalOvertimeSalary: 1,
+                        totalSalary: 1
+                    }
+                }
+            ]).toArray();
+
+            res.send(aggregateResult);
+        })
+
+
+
+        // month wise
+
+
+        app.get('/employeeSummarymonth/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const aggregateResult = await AllWorkSheetCollection.aggregate([
+                {
+                    $addFields: {
+                        month: { $month: { $dateFromString: { dateString: '$date' } } }
+                    }
+                },
+                {
+                    $group: {
+                        _id: { $dateToString: { format: '%Y-%m', date: { $dateFromString: { dateString: '$date' } } } },
+                        totalWorks: { $sum: 1 },
+                        totalHours: { $sum: { $toInt: '$hours' } },
+                        totaloverHours: { $sum: { $toInt: '$overtime' } },
+                        totalMainSalary: { $sum: { $toDouble: '$mainSalary' } },
+                        totalOvertimeSalary: { $sum: { $toDouble: '$overtimeSalary' } },
+                        totalUniqueDaysWorked: { $addToSet: { $dateToString: { format: '%Y-%m-%d', date: { $dateFromString: { dateString: '$date' } } } } }
+                    }
+                },
+                {
+                    $addFields: {
+                        totalSalary: { $add: ['$totalMainSalary', '$totalOvertimeSalary'] }
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        month: '$_id',
+                        totalWorks: 1,
+                        totalHours: 1,
+                        totaloverHours: 1,
+                        totalMainSalary: 1,
+                        totalOvertimeSalary: 1,
+                        totalSalary: 1,
+                        totalUniqueDaysWorked: { $size: '$totalUniqueDaysWorked' }
+                    }
+                },
+                {
+                    $sort: { month: 1 } // Sort the results by month in ascending order (optional)
+                }
+            ]).toArray();
+
+            res.send(aggregateResult);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // All Create api works here
         // auth related api
